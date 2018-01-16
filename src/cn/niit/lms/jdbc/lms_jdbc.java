856ec01528fc41909bc84c908b1b7dce5c5c935d
GustPig <245405027@qq.com>
2018-01-16 10:03:28 +0800

    修改了lms_jdbc包，usermanage.java调用lms_jdbc包实现从数据库输出数据的功能。开始做页面表格的数据填充

--- src/cn/niit/lms/jdbc/lms_jdbc.java
+++ src/cn/niit/lms/jdbc/lms_jdbc.java
@@ -6,20 +6,63 @@ import java.util.*;
 public class lms_jdbc {
 	private final static String driver="com.mysql.jdbc.Driver";
 	private final static String url="jdbc:mysql://localhost:3306/lmsdb";
-	static Connection con=null;
-	Statement stmt=null;
-		
+	private	static Connection conn=null;
+	private	static Statement stmt=null;
+	private	static String sql;	
+	public static ResultSet rs; 
+	
+	//jdbc
 	public static boolean creatConnection(){
 		try{
-			Class.forName(driver);
-			con=DriverManager.getConnection(url,"root","password");
+			Class.forName(driver); //注册JDBC驱动
+			conn=DriverManager.getConnection(url,"root","password"); //连接数据库
+			stmt = conn.createStatement(); // 实例化Statement对
+			System.out.println("数据库连接成功");
 			return true;
 		}
 		catch(Exception e){
 			System.out.println("数据库连接失败"+e.toString());
 			return false;
 		}
+	}	
+	
+	//执行函数
+	public static void Query(String mysql){
+		sql = mysql;//修改查询语句
+		try {
+			rs = stmt.executeQuery(sql);//执行
+			System.out.println("执行成功");
+		} catch (SQLException e) {
+			System.out.println("执行失败");
+			e.printStackTrace();
+		}
 	}
+	
+	//清理函数
+	public static void sqlClose(){
+		try {
+			rs.close();
+			stmt.close();
+			conn.close();
+			System.out.println("清理成功");
+		} catch (SQLException se) {
+			// TODO Auto-generated catch block
+			se.printStackTrace();
+		} catch (Exception e){
+			e.printStackTrace();
+		} finally{
+			try{
+				if(stmt != null) stmt.close();
+			} catch(SQLException se2){
+			}
+			try{
+				if(conn != null) conn.close();
+			} catch(SQLException se){
+				se.printStackTrace();
+			}
+		}
+	}
+	
 	public static void main(String[] arg){
 		boolean sta=creatConnection();
 		if(sta)
--- src/cn/niit/lms/manage/UserManage.java
+++ src/cn/niit/lms/manage/UserManage.java
@@ -1,35 +1,68 @@
 package cn.niit.lms.manage;
 
 import java.sql.*;
+import cn.niit.lms.jdbc.*;
 
 public class UserManage {
-	private int UID; // 主键
-	private String uname; //姓名
-	private String role; //类别（student/teacher）
-	private String phone; //手机号
-	private String pwd; //密码
-	private String email; //邮箱
-	private String address; //地址
-	private String dob; // 生日
-	private boolean gender; //性别
-	private int fine; //罚金
+	private static int UID; // 主键
+	private static String uname; //姓名
+	private static String role; //类别（student/teacher）
+	private static String phone; //手机号
+	private static String pwd; //密码
+	private static String email; //邮箱
+	private static String address; //地址
+	private static String dob; // 生日
+	private static boolean gender; //性别
+	private static int fine; //罚金
 	
-	private final static String driver="com.mysql.jdbc.Driver";
-	private final static String url="jdbc:mysql://localhost:3306/lmsdb";
-	static Connection con=null;
-	Statement stmt=null;
-	//jdbc
-	public static boolean creatConnection(){
-		try{
-			Class.forName(driver);
-			con=DriverManager.getConnection(url,"root","password");
-			return true;
-		}
-		catch(Exception e){
-			System.out.println("数据库连接失败"+e.toString());
-			return false;
-		}
-	}	
-	
+	private static ResultSet rs; //创建结果集
 
+	//展开结果集数据库
+	public static void Show(){
+		lms_jdbc.creatConnection();
+		lms_jdbc.Query("Select * from users");
+		try {
+			while(lms_jdbc.rs.next()){
+				//字段检索
+				UID = lms_jdbc.rs.getInt("UID");
+				uname = lms_jdbc.rs.getString("uname");
+				role = lms_jdbc.rs.getString("role");
+				phone = lms_jdbc.rs.getString("phone");
+				pwd = lms_jdbc.rs.getString("pwd");
+				email = lms_jdbc.rs.getString("email");
+				address = lms_jdbc.rs.getString("address");
+				dob = lms_jdbc.rs.getString("dob"); 
+				gender = lms_jdbc.rs.getBoolean("gender");
+				
+				//输出数据
+				System.out.println("UID:" + UID);
+				System.out.println("Name:" + uname);
+				if(role=="s")
+					System.out.println("Role: Student");
+				else if(role=="t")
+					System.out.println("Role: Teacher");
+				else
+					System.out.println("Role: Librarian");
+				System.out.println("Phone:" + phone);
+				System.out.println("Password:" + pwd);
+				System.out.println("E-mail:" + email);
+				System.out.println("Address:" + address);
+				System.out.println("Brithday:" + dob);
+				
+				if(gender)
+					System.out.println("Gender: man");
+				else
+					System.out.println("Gender: women");
+			}
+			//关闭sql
+			lms_jdbc.sqlClose();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] arg){
+		Show();
+	}
 }
