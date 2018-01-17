4902810f03f414a355caafb6dd1634bb6e6a7715
Damonnnnnnn6 <916474516@qq.com>
2018-01-17 13:18:12 +0800

    2018-1-17 13:18:08

--- src/cn/niit/lms/bookmanage/AddBookServlet.java
+++ src/cn/niit/lms/bookmanage/AddBookServlet.java
@@ -3,6 +3,7 @@ package cn.niit.lms.bookmanage;
 import java.io.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.util.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,8 +11,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.*;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+import com.sun.org.apache.xalan.internal.xsltc.compiler.sym;
+
 import cn.niit.lms.jdbc.*;
-import jdk.nashorn.internal.ir.Flags;
 
 /**
  * Servlet implementation class AddBookServlet
@@ -31,7 +37,8 @@ public class AddBookServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		
@@ -40,24 +47,51 @@ public class AddBookServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String ISBN = request.getParameter("ISBN");
 		String Title = request.getParameter("Title");
 		String Author = request.getParameter("Author");
 		String Category = request.getParameter("Category");
 		String sPrice = request.getParameter("Price");
+		System.out.println("Sprice  "+sPrice);
 		Float Price = Float.parseFloat(sPrice);
 		File Cover= new File(request.getParameter("Cover"));
 		Connection conn = null;  
-        PreparedStatement ps = null;
+        PreparedStatement pstmt = null;
         FileInputStream fis;
         System.out.println("This AddBookServlet  Category: "+Category);
+        
+        try {        	
+        	DiskFileItemFactory factory=new DiskFileItemFactory();
+        	ServletFileUpload fu=new ServletFileUpload(factory);
+        	List list = fu.parseRequest(request);
+        	Iterator it = list.iterator();
+        	while (it.hasNext()) 
+        	{
+	        	 FileItem item = (FileItem) it.next();
+	        	 if (item.isFormField()){
+	        		 //formname是name值   fromcontent是value值   
+	        	  String formname=item.getFieldName();//获取name属性的值
+	        	  System.out.println("name值  "+formname);
+	        	  String formcontent=item.getString();//获取value属性的值
+	        	 }
+        	}
+        	
+        	
+        				
+		} catch (FileUploadException e1) {
+			// TODO Auto-generated catch block
+			System.out.println("Error in ServletFileUpload" + e1);
+		}
+        /*
         try {
         	conn = JDBCUtils.getConnection();
         	System.out.println("Connection Successfully!");
         	String sql="insert into isbn_books(ISBN,title,author,category,price,cover) values(?,?,?,?,?,?)";
-        	ps=conn.prepareStatement(sql);
+        	pstmt=conn.prepareStatement(sql);
+        	/*
         	ps.setString(1, ISBN);
         	ps.setString(2, Title);
         	ps.setString(3, Author);
@@ -75,12 +109,10 @@ public class AddBookServlet extends HttpServlet {
 
     		}
         	conn.close();
-        	ps.close();
-        		
+        	ps.close();        		
 		} catch (Exception e) {
 			System.out.println("Error in connection : " + e);
 			System.out.println("Error!" + e);
-		}
+		}*/
 	}
-
 }
